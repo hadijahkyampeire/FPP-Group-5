f96ab52e11eeb4aa3000cffdc34dd89136182356
hadijahkyampeire <hadijah315@gmail.com>
2024-11-26 19:39:08 -0600

    fix prog1 bounds

--- Lesson2-Home-work/src/Prog1.java
+++ Lesson2-Home-work/src/Prog1.java
@@ -5,11 +5,11 @@ public class Prog1 {
         double PI = Math.PI;
 
         Random random = new Random();
-        int randomIntX = random.nextInt(1, 9);
+        int randomIntX = random.nextInt(1, 10);
         double comp1 = Math.pow(PI, randomIntX);
-        System.out.printf("Comp1 value is %.2f", comp1);
-
-        int randomIntY = random.nextInt(3, 14);
+        System.out.printf("Comp1 value is %.2f \n", comp1);
+        
+        int randomIntY = random.nextInt(3, 15);
         double comp2 = Math.pow(randomIntY, PI);
         System.out.printf("Comp2 value is %.2f", comp2);
     }
